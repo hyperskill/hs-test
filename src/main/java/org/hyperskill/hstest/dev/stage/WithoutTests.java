1c679c082a48a2a75b98fa0331582d9af34592ac
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-02-18 17:41:14 +0300

    Revert "Add class WithoutTests to make stages without tests (in v2 also)"
    This reverts commit 7c03230a6864cf385cb3f5db532b4a1914bf0877.

--- src/main/java/org/hyperskill/hstest/dev/stage/WithoutTests.java
+++
@@ -1,8 +0,0 @@
-package org.hyperskill.hstest.dev.stage;
-
-public class WithoutTests extends BaseStageTest {
-    public WithoutTests(Class<?> testedClass) throws Exception {
-        super(testedClass.getMethod("main", String[].class));
-        isWithoutTests = true;
-    }
-}
--- src/main/java/org/hyperskill/hstest/v2/stage/BaseStageTest.java
+++ src/main/java/org/hyperskill/hstest/v2/stage/BaseStageTest.java
@@ -25,7 +25,6 @@ public abstract class BaseStageTest<ClueType> implements StageTest {
     private final Object testedObject;
     private final Method testedMethod;
     protected boolean isTestingMain = false; // TODO potentially it is possible to make it final
-    protected boolean isWithoutTests = false;
 
     private final boolean overrodeTestCases;
     private final boolean overrodePredefinedIO;
@@ -79,7 +78,7 @@ public abstract class BaseStageTest<ClueType> implements StageTest {
         overrodeCheck = !myName.equals(checkOwner);
         overrodeSolve = !myName.equals(solveOwner);
 
-        if (!overrodeTestCases && !overrodePredefinedIO && !isWithoutTests) {
+        if (!overrodeTestCases && !overrodePredefinedIO) {
             throw new Exception("No tests provided: override " +
                 "generateTestCases and/or generatePredefinedInputOutput");
         }
