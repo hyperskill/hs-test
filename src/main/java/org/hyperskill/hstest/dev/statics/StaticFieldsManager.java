cb77063f71473040e2daf7cd9ba84f6fd3c69cdf
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-03-06 01:31:32 +0300

    Add ability to save and rewrite final fields

--- src/main/java/org/hyperskill/hstest/dev/statics/StaticFieldsManager.java
+++ src/main/java/org/hyperskill/hstest/dev/statics/StaticFieldsManager.java
@@ -1,5 +1,7 @@
 package org.hyperskill.hstest.dev.statics;
 
+import sun.misc.Unsafe;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
@@ -17,18 +19,31 @@ public class StaticFieldsManager {
         return className.substring(0, className.indexOf("."));
     }
 
+    private static void disableWarning() {
+        try {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            Unsafe u = (Unsafe) theUnsafe.get(null);
+
+            Class cls = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field logger = cls.getDeclaredField("logger");
+            u.putObjectVolatile(cls, u.staticFieldOffset(logger), null);
+        } catch (Exception e) {
+            // ignore
+        }
+    }
+
     private static Map<Field, Object> saveFieldsForClass(Class clazz) throws Exception {
         Map<Field, Object> savedFields = new HashMap<>();
         Field[] allFields = clazz.getDeclaredFields();
         try {
             for (Field field : allFields) {
                 boolean isStatic = Modifier.isStatic(field.getModifiers());
-                boolean isFinal = Modifier.isFinal(field.getModifiers());
-                boolean isPublic = Modifier.isPublic(field.getModifiers());
-                if (isStatic && !isFinal) {
-                    if (!isPublic) {
-                        field.setAccessible(true);
-                    }
+                if (isStatic) {
+                    field.setAccessible(true);
+                    Field modifiersField = Field.class.getDeclaredField("modifiers");
+                    modifiersField.setAccessible(true);
+                    modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
                     Object value = ObjectsCloner.cloneObject(field.get(null));
                     savedFields.put(field, value);
                 }
@@ -41,6 +56,7 @@ public class StaticFieldsManager {
     }
 
     public static void saveStaticFields(String packageName) throws Exception {
+        disableWarning();
         List<Class<?>> userClasses = ClassSearcher.getClassesForPackage(packageName);
         for (Class clazz : userClasses) {
             String className = clazz.getCanonicalName();
