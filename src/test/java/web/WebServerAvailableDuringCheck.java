98093a857f3de722f36dffb7dda14d31975fafb7
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-03-11 10:42:33 +0300

    Add tests on WebServer availability during testing and checking

---
+++ src/test/java/web/WebServerAvailableDuringCheck.java
@@ -0,0 +1,36 @@
+package web;
+
+import org.hyperskill.hstest.dev.common.Utils;
+import org.hyperskill.hstest.dev.mocks.web.WebPage;
+import org.hyperskill.hstest.dev.mocks.web.WebServerMock;
+import org.hyperskill.hstest.dev.stage.MainMethodTest;
+import org.hyperskill.hstest.dev.testcase.CheckResult;
+import org.hyperskill.hstest.dev.testcase.TestCase;
+
+import java.util.List;
+
+public class WebServerAvailableDuringCheck extends MainMethodTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public WebServerAvailableDuringCheck() throws Exception {
+        super(WebServerAvailableDuringCheck.class);
+    }
+
+    @Override
+    public List<TestCase> generateTestCases() {
+        return List.of(
+            new TestCase()
+            .runWith(new WebServerMock(45678)
+                .setPage("/123", new WebPage().setContent("234")))
+        );
+    }
+
+    @Override
+    public CheckResult check(String reply, Object clue) {
+        return new CheckResult("234".equals(Utils.getUrlPage("http://127.0.0.1:45678/123")));
+    }
+
+}
