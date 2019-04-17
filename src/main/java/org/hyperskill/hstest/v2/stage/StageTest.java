3bd43ed1dc0a3afcc0d3d291c98421c0af0caffd
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-04-18 01:12:57 +0300

    Remove deprecated packages

--- src/main/java/org/hyperskill/hstest/v2/stage/StageTest.java
+++
@@ -1,9 +0,0 @@
-package org.hyperskill.hstest.v2.stage;
-
-public interface StageTest {
-
-    /**
-     * Any stage can be started automatically by a testing framework under the hood or manually
-     */
-    void start();
-}
