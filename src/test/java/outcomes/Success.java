006c313f288f78794dad1a24a947569e2f43a55d
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-04-19 03:22:08 +0300

    Add check on non-null class fields during generating tests

--- src/test/java/outcomes/Success.java
+++ src/test/java/outcomes/Success.java
@@ -8,21 +8,26 @@ import org.hyperskill.hstest.dev.testcase.TestCase;
 import java.util.Arrays;
 import java.util.List;
 
-public class Success extends MainMethodTest {
+public class Success extends MainMethodTest<String> {
 
     public Success() throws Exception {
         super(WithoutException.class);
     }
 
+    String succ = "123";
+
     @Override
-    public List<TestCase> generateTestCases() {
+    public List<TestCase<String>> generateTestCases() {
         return Arrays.asList(
-            new TestCase()
+            new TestCase<String>().setAttach(succ)
         );
     }
 
     @Override
-    public CheckResult check(String reply, Object clue) {
+    public CheckResult check(String reply, String clue) {
+        if (clue == null) {
+            System.out.println(0 / 0);
+        }
         return CheckResult.TRUE;
     }
 }
