e179e1b8b9100ad9198e3f9ee06ed746064c5f49
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-06-11 13:57:04 +0300

    Add tests on custom checker

---
+++ src/test/java/outcomes/TestCustomChecker.java
@@ -0,0 +1,42 @@
+package outcomes;
+
+import org.hyperskill.hstest.dev.stage.BaseStageTest;
+import org.hyperskill.hstest.dev.testcase.CheckResult;
+import org.hyperskill.hstest.dev.testcase.TestCase;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestCustomChecker extends BaseStageTest<String> {
+
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+
+    public TestCustomChecker() {
+        super(TestCustomChecker.class);
+    }
+
+    @Override
+    public List<TestCase<String>> generate() {
+        return Arrays.asList(
+            new TestCase<String>()
+                .addArguments("-in", "123", "-out", "234")
+                .setAttach("4\n-in\n123\n-out\n234\n")
+                .setCheckFunc(this::customCheck),
+
+            new TestCase<String>()
+                .addArguments("-in", "435")
+                .addArguments("-out", "567", "789")
+                .setAttach("5\n-in\n435\n-out\n567\n789\n")
+                .setCheckFunc(this::customCheck)
+        );
+    }
+
+    private CheckResult customCheck(String reply, String clue) {
+        return new CheckResult(reply.equals(clue));
+    }
+}
---
+++ src/test/java/outcomes/TestCustomCheckerFail.java
@@ -0,0 +1,54 @@
+package outcomes;
+
+import org.hyperskill.hstest.dev.stage.BaseStageTest;
+import org.hyperskill.hstest.dev.testcase.CheckResult;
+import org.hyperskill.hstest.dev.testcase.TestCase;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestCustomCheckerFail extends BaseStageTest<String> {
+
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+
+    public TestCustomCheckerFail() {
+        super(TestCustomCheckerFail.class);
+    }
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void before() {
+        exception.expect(AssertionError.class);
+        exception.expectMessage("Fatal error during testing, please send the report to Hyperskill team.");
+        exception.expectMessage("Can't check result: override \"check\" method");
+    }
+
+    @Override
+    public List<TestCase<String>> generate() {
+        return Arrays.asList(
+            new TestCase<String>()
+                .addArguments("-in", "123", "-out", "234")
+                .setAttach("4\n-in\n123\n-out\n234\n")
+                .setCheckFunc(this::customCheck),
+
+            new TestCase<String>()
+                .addArguments("-in", "435")
+                .addArguments("-out", "567", "789")
+                .setAttach("5\n-in\n435\n-out\n567\n789\n")
+        );
+    }
+
+    private CheckResult customCheck(String reply, String clue) {
+        return new CheckResult(reply.equals(clue));
+    }
+}
