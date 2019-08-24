9e93e6773e3f623f5cc85a151bc607489ea73dc7
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-08-24 21:16:49 +0300

    Remove todo

--- src/main/java/org/hyperskill/hstest/dev/dynamic/input/SystemInMock.java
+++ src/main/java/org/hyperskill/hstest/dev/dynamic/input/SystemInMock.java
@@ -3,24 +3,30 @@ package org.hyperskill.hstest.dev.dynamic.input;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
+import java.util.List;
+import java.util.function.Function;
 
 public class SystemInMock extends InputStream {
     private StringReader currentReader;
+
+    List<Function<String, String>> inputTexts;
 
     void provideText(String text) {
         currentReader = new StringReader(text);
     }
 
+    void setTexts(List<Function<String, String>> texts) {
+        inputTexts = texts;
+        currentReader = new StringReader("");
+    }
+
     @Override
     public int read() throws IOException {
         int character = currentReader.read();
-        if (character == -1)
-            handleEmptyReader();
-        return character;
-    }
+        if (character == -1) {
 
-    private void handleEmptyReader() {
-        // TODO
+        }
+        return character;
     }
 
 }
