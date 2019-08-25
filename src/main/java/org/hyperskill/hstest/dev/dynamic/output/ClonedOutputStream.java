c6d7bc5e7081771e3163f814cde527fcd87bc055
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-08-25 03:20:40 +0300

    Add comments to output streams

--- src/main/java/org/hyperskill/hstest/dev/dynamic/output/ClonedOutputStream.java
+++ src/main/java/org/hyperskill/hstest/dev/dynamic/output/ClonedOutputStream.java
@@ -6,8 +6,16 @@ import java.io.OutputStream;
 
 public class ClonedOutputStream extends OutputStream {
 
+    // original stream used to actually see
+    // the test in the console and nothing else
     private final OutputStream originalStream;
+
+    // cloned stream used to collect all output
+    // from the test and redirect to check function
     final ByteArrayOutputStream clonedStream = new ByteArrayOutputStream();
+
+    // dynamic stream used to collect output between
+    // dynamic input calls in SystemInMock
     final ByteArrayOutputStream dynamicStream = new ByteArrayOutputStream();
 
     ClonedOutputStream(OutputStream originalStream) {
