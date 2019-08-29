0ba10e96ef8aa48c4f547d46fb278f307a4ab466
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-08-29 16:23:42 +0300

    Add test checking curr test num

---
+++ src/test/java/outcomes/TestCurrTestNum.java
@@ -0,0 +1,47 @@
+package outcomes;
+
+import org.hyperskill.hstest.dev.stage.BaseStageTest;
+import org.hyperskill.hstest.dev.testcase.CheckResult;
+import org.hyperskill.hstest.dev.testcase.TestCase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class TestCurrTestNum extends BaseStageTest<Integer> {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int test = s.nextInt();
+        int testNum = BaseStageTest.getCurrTestRun().getTestNum();
+        if (test != testNum) {
+            throw new NullPointerException();
+        }
+    }
+
+    public TestCurrTestNum() {
+        super(TestCurrTestNum.class);
+    }
+
+    @Override
+    public List<TestCase<Integer>> generate() {
+        return Arrays.asList(
+            new TestCase<Integer>()
+                .setInput("1")
+                .setAttach(1),
+
+            new TestCase<Integer>()
+                .setInput("2")
+                .setAttach(2)
+        );
+    }
+
+    @Override
+    public CheckResult check(String reply, Integer attach) {
+        int testNum = BaseStageTest.getCurrTestRun().getTestNum();
+        if (attach != testNum) {
+            throw new NullPointerException();
+        }
+        return CheckResult.TRUE;
+    }
+}
