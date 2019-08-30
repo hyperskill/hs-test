7a5bb3041eaab7ec86e5dde637cc2f94c7c65263
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-08-30 19:59:51 +0300

    Add tests on dynamic input and output

---
+++ src/test/java/outcomes/TestDynamicOutput.java
@@ -0,0 +1,66 @@
+package outcomes;
+
+import org.hyperskill.hstest.dev.stage.BaseStageTest;
+import org.hyperskill.hstest.dev.testcase.CheckResult;
+import org.hyperskill.hstest.dev.testcase.TestCase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class TestDynamicOutput extends BaseStageTest<String> {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1");
+        System.out.println("2");
+        String line3 = scanner.nextLine();
+        if (!line3.equals("3")) {
+            int x = 0/0;
+        }
+        System.out.println("5");
+        String line4 = scanner.nextLine();
+        if (!line4.equals("4")) {
+            int x = 0/0;
+        }
+        System.out.println("6");
+        String line7 = scanner.nextLine();
+        if (!line7.equals("7")) {
+            int x = 0/0;
+        }
+        String line8 = scanner.nextLine();
+        if (!line8.equals("8")) {
+            int x = 0/0;
+        }
+    }
+
+    public TestDynamicOutput() {
+        super(TestDynamicOutput.class);
+    }
+
+    @Override
+    public List<TestCase<String>> generate() {
+        return Arrays.asList(
+            new TestCase<String>()
+                .addInput(out -> {
+                    System.err.println("in1");
+                    if (!out.equals("1\n2\n")) {
+                        int x = 0/0;
+                    }
+                    return "3\n4";
+                })
+                .addInput(out -> {
+                    System.err.println("in2");
+                    if (!out.equals("5\n6\n")) {
+                        int x = 0/0;
+                    }
+                    return "7\n8\n";
+                })
+        );
+    }
+
+    @Override
+    public CheckResult check(String reply, String attach) {
+        return CheckResult.TRUE;
+    }
+}
---
+++ src/test/java/outcomes/TestMultipleScanners.java
@@ -0,0 +1,63 @@
+package outcomes;
+
+import org.hyperskill.hstest.dev.stage.BaseStageTest;
+import org.hyperskill.hstest.dev.testcase.CheckResult;
+import org.hyperskill.hstest.dev.testcase.TestCase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class TestMultipleScanners extends BaseStageTest<String> {
+
+    private static void in(String s) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        if (!line.equals(s)) {
+            throw new NullPointerException();
+        }
+    }
+
+    private static void out(String s) {
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        out("1");
+        out("2");
+        in("3");
+        out("5");
+        in("4");
+        out("6");
+        in("7");
+        in("8");
+    }
+
+    public TestMultipleScanners() {
+        super(TestMultipleScanners.class);
+    }
+
+    @Override
+    public List<TestCase<String>> generate() {
+        return Arrays.asList(
+            new TestCase<String>()
+                .addInput(out -> {
+                    if (!out.equals("1\n2\n")) {
+                        int x = 0/0;
+                    }
+                    return "3\n4";
+                })
+                .addInput(out -> {
+                    if (!out.equals("5\n6\n")) {
+                        int x = 0/0;
+                    }
+                    return "7\n8\n";
+                })
+        );
+    }
+
+    @Override
+    public CheckResult check(String reply, String attach) {
+        return CheckResult.TRUE;
+    }
+}
