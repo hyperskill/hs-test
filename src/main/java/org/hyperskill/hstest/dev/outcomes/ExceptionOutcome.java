0f038053ffe3bfa491d7542b0107b8e970560f4d
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-08-30 20:25:40 +0300

    Correct error texts

--- src/main/java/org/hyperskill/hstest/dev/outcomes/ExceptionOutcome.java
+++ src/main/java/org/hyperskill/hstest/dev/outcomes/ExceptionOutcome.java
@@ -17,15 +17,17 @@ public class ExceptionOutcome extends Outcome {
 
             errorText = "Probably you have nextInt() (or similar Scanner method) " +
                 "followed by nextLine() - in this situation nextLine() often gives an " +
-                "empty string and the second nextLine() gives correct string.";
+                "empty string and another one nextLine() call gives correct string.";
 
         } else if (cause instanceof NoSuchElementException
             && stackTrace.contains("java.util.Scanner")) {
 
-            errorText = "Maybe you created more than one instance of Scanner? " +
-                "You should use a single Scanner in program. " +
-                "If not, this type of exception also happens if you " +
-                "run out of input (tried to read more than expected).";
+            errorText = "Probably your program run out of input " +
+                "(Scanner tried to read more than expected). " +
+                "If you are sure it's not, this type of exception " +
+                "also happens if you created more than one Scanner object " +
+                "(it is preferred to use a single Scanner in program).";
+
         }
     }
 
