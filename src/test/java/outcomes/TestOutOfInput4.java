d01b9adb3c2d886293cc1dc4aed858cecca422c5
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-08-30 03:46:08 +0300

    Add another test on out of input

---
+++ src/test/java/outcomes/TestOutOfInput4.java
@@ -0,0 +1,56 @@
+package outcomes;
+
+import org.hyperskill.hstest.dev.stage.BaseStageTest;
+import org.hyperskill.hstest.dev.testcase.CheckResult;
+import org.hyperskill.hstest.dev.testcase.TestCase;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+
+public class TestOutOfInput4 extends BaseStageTest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 6; i++) {
+            System.out.println(scanner.nextInt());
+        }
+    }
+
+    public TestOutOfInput4() {
+        super(TestOutOfInput4.class);
+    }
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void before() {
+        exception.expect(AssertionError.class);
+        exception.expectMessage("Exception in test #1");
+        exception.expectMessage("Maybe you created more than one instance of Scanner?");
+        exception.expectMessage("java.util.NoSuchElementException");
+        exception.expectMessage(not(containsString("Fatal error")));
+    }
+
+    @Override
+    public List<TestCase> generate() {
+        return Arrays.asList(
+            new TestCase<String>()
+                .addInput(2, o -> "1\n2")
+                .addInput(o -> "2")
+        );
+    }
+
+    @Override
+    public CheckResult check(String reply, Object attach) {
+        return CheckResult.TRUE;
+    }
+
+}
