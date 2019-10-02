6824b022349a4cc4bb992306f83a8fa015cbfc8e
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-10-02 21:20:46 +0300

    Remove v4 and v5

--- src/main/java/org/hyperskill/hstest/v4/stage/MainMethodTest.java
+++
@@ -1,9 +0,0 @@
-package org.hyperskill.hstest.v4.stage;
-
-public abstract class MainMethodTest<AttachType> extends BaseStageTest<AttachType> {
-
-    public MainMethodTest(Class<?> testedClass) throws Exception {
-        super(testedClass.getMethod("main", String[].class));
-        isTestingMain = true;
-    }
-}
--- src/main/java/org/hyperskill/hstest/v5/exception/WrongAnswerException.java
+++
@@ -1,7 +0,0 @@
-package org.hyperskill.hstest.v5.exception;
-
-public class WrongAnswerException extends Exception {
-    public WrongAnswerException(String errorText) {
-        super(errorText);
-    }
-}
