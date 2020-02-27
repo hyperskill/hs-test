f1813cbf28361a1d529347f0b1f345db85a12e64
aaaaaa2493 <aaaaaa2493@yandex.ru>
2020-02-27 22:36:08 +0300

    Add additional locale tests

--- src/test/java/outcomes/TestPrintNumberWithDot.java
+++ src/test/java/outcomes/TestPrintNumberWithDot.java
@@ -4,7 +4,8 @@ import org.hyperskill.hstest.v7.stage.StageTest;
 import org.hyperskill.hstest.v7.testcase.CheckResult;
 import org.hyperskill.hstest.v7.testcase.TestCase;
 
-import java.util.Arrays;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -33,6 +34,30 @@ class TestPrintNumberWithDotMain {
             case 6:
                 System.out.format("%f", f);
                 break;
+            case 7:
+                PrintWriter printWriter1 = new PrintWriter(System.out);
+                printWriter1.printf("%f", d);
+                printWriter1.flush();
+                break;
+            case 8:
+                PrintWriter printWriter2 = new PrintWriter(System.out);
+                printWriter2.printf("%f", f);
+                printWriter2.flush();
+                break;
+            case 9:
+                System.out.println(Double.parseDouble("1.234"));
+                break;
+            case 10:
+                System.out.println(Float.parseFloat("1.234"));
+                break;
+            case 11:
+                Scanner scanner1 = new Scanner("1.234");
+                System.out.println(scanner1.nextDouble());
+                break;
+            case 12:
+                Scanner scanner2 = new Scanner("1.234");
+                System.out.println(scanner2.nextFloat());
+                break;
         }
     }
 }
@@ -45,14 +70,11 @@ public class TestPrintNumberWithDot extends StageTest<String> {
 
     @Override
     public List<TestCase<String>> generate() {
-        return Arrays.asList(
-            new TestCase<String>().setInput("1"),
-            new TestCase<String>().setInput("2"),
-            new TestCase<String>().setInput("3"),
-            new TestCase<String>().setInput("4"),
-            new TestCase<String>().setInput("5"),
-            new TestCase<String>().setInput("6")
-        );
+        List<TestCase<String>> tests = new ArrayList<>();
+        for (int i = 1; i <= 12; i++) {
+            tests.add(new TestCase<String>().setInput("" + i));
+        }
+        return tests;
     }
 
     @Override
