f9e4d63d129272e4ac5bb978f171147261f0332c
aaaaaa2493 <aaaaaa2493@yandex.ru>
2020-03-31 21:23:10 +0300

    Add ability to throw WrongAnswer and TestPassed in dynamic input

---
+++ src/test/java/outcomes/WrongAnswerThrownInDynamicInput1.java
@@ -0,0 +1,68 @@
+package outcomes;
+
+import org.hyperskill.hstest.v7.exception.outcomes.WrongAnswer;
+import org.hyperskill.hstest.v7.stage.StageTest;
+import org.hyperskill.hstest.v7.testcase.CheckResult;
+import org.hyperskill.hstest.v7.testcase.TestCase;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+
+
+class WrongAnswerThrownInDynamicInput1Main {
+    public static void main(String[] args) {
+        System.out.print("1");
+        new Scanner(System.in).next();
+    }
+}
+
+public class WrongAnswerThrownInDynamicInput1 extends StageTest<Boolean> {
+
+    public WrongAnswerThrownInDynamicInput1() {
+        super(WrongAnswerThrownInDynamicInput1Main.class);
+    }
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void before() {
+        exception.expect(AssertionError.class);
+        exception.expectMessage("Wrong answer in test #1\n\n" +
+            "Add input test 1");
+        exception.expectMessage(not(containsString("Fatal error")));
+    }
+
+    @Override
+    public List<TestCase<Boolean>> generate() {
+        return Arrays.asList(
+            new TestCase<Boolean>()
+                .addInput(out -> {
+                    if (out.equals("1")) {
+                        throw new WrongAnswer("Add input test 1");
+                    }
+                    return "2";
+                }),
+
+            new TestCase<Boolean>()
+                .addInput(out -> {
+                    if (out.equals("2")) {
+                        throw new WrongAnswer("Add input test 2");
+                    }
+                    return "1";
+                })
+        );
+    }
+
+    @Override
+    public CheckResult check(String reply, Boolean attach) {
+        return CheckResult.correct();
+    }
+}
