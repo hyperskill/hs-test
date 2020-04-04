e25a677343c22cc40eadaa86d51a987144f9f60f
aaaaaa2493 <aaaaaa2493@yandex.ru>
2020-04-04 22:46:51 +0300

    Add docstring comments on DynamicInput class

--- src/main/java/org/hyperskill/hstest/v7/dynamic/input/DynamicInput.java
+++ src/main/java/org/hyperskill/hstest/v7/dynamic/input/DynamicInput.java
@@ -13,9 +13,41 @@ import java.util.function.Function;
 
 import static org.hyperskill.hstest.v7.common.Utils.cleanText;
 
+/**
+ * Interface for creating tests with dynamic input.
+ * It is preferred to use dynamic input rather than static.
+ * Static input means it's determined before the start of the program,
+ * while dynamic input is calculated in the middle of testing process.
+ *
+ * Dynamic testing has some benefits over tests with static input:
+ * 1) You are able to test small parts of the output compared to checking
+ *    whole output. Parsing it to determine if the program written correctly
+ *    may be more error-prone rather than parsing small pieces of the output.
+ * 2) Some programs aren't testable with static input because the input may
+ *    be dependent on the previous output (for example, Tic-Tac-Toe game).
+ */
 public interface DynamicInput {
+    /**
+     * Method that provides dynamic input. Designed to be used with
+     * TestedProgram class to be able to partially execute tested program.
+     * You can also partially execute multiple programs at the same time.
+     * @return CheckResult object at the result of the testing. Return null
+     *         if output needs additional testing using StageTest.check method.
+     *         Converting old dynamic input to the new one requires returning null
+     *         so written tests remain backwards-compatible but it's preferred
+     *         to check user's output in DynamicInput.handle method and return
+     *         a CheckResult object.
+     */
     CheckResult handle();
 
+    /**
+     * Converter from old way of constructing dynamic input (multiple methods)
+     * to new way (using single method)
+     * @param testedClass class that is tested
+     * @param args arguments for the testedClass's main method
+     * @param inputFuncs old way of constructing dynamic input
+     * @return DynamicInput's single method that provides dynamic input.
+     */
     static DynamicInput toDynamicInput(Class<?> testedClass, List<String> args,
                                        List<DynamicInputFunction> inputFuncs) {
 
