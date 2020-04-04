5430ae9ac6e89d95761355df40e6ae01620759ac
aaaaaa2493 <aaaaaa2493@yandex.ru>
2020-04-05 02:54:01 +0300

    Add time limit checking

--- src/main/java/org/hyperskill/hstest/v7/testing/runner/AsyncMainMethodRunner.java
+++ src/main/java/org/hyperskill/hstest/v7/testing/runner/AsyncMainMethodRunner.java
@@ -3,15 +3,46 @@ package org.hyperskill.hstest.v7.testing.runner;
 import org.hyperskill.hstest.v7.dynamic.input.DynamicInput;
 import org.hyperskill.hstest.v7.dynamic.output.SystemOutHandler;
 import org.hyperskill.hstest.v7.exception.outcomes.TestPassed;
+import org.hyperskill.hstest.v7.exception.outcomes.TimeLimitException;
 import org.hyperskill.hstest.v7.exception.outcomes.WrongAnswer;
 import org.hyperskill.hstest.v7.stage.StageTest;
 import org.hyperskill.hstest.v7.testcase.CheckResult;
 import org.hyperskill.hstest.v7.testcase.TestCase;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.hyperskill.hstest.v7.common.ProcessUtils.newDaemonThreadPool;
 import static org.hyperskill.hstest.v7.testcase.CheckResult.correct;
 import static org.hyperskill.hstest.v7.testcase.CheckResult.wrong;
 
 public class AsyncMainMethodRunner implements TestRunner {
+
+    private CheckResult runMain(TestCase<?> testCase) {
+        int timeLimit = testCase.getTimeLimit();
+
+        ExecutorService executorService = newDaemonThreadPool(1);
+        Future<CheckResult> future = executorService
+            .submit(() -> testCase.getDynamicInput().handle());
+
+        try {
+            if (timeLimit <= 0) {
+                return future.get();
+            } else {
+                return future.get(timeLimit, TimeUnit.MILLISECONDS);
+            }
+        } catch (TimeoutException ex) {
+            StageTest.getCurrTestRun().setErrorInTest(new TimeLimitException(timeLimit));
+        } catch (Throwable ex) {
+            StageTest.getCurrTestRun().setErrorInTest(ex);
+        } finally {
+            executorService.shutdownNow();
+        }
+
+        return null;
+    }
 
     @Override
     public <T> CheckResult test(TestCase<T> testCase) {
@@ -21,18 +52,19 @@ public class AsyncMainMethodRunner implements TestRunner {
             testCase.setDynamicInput(converted);
         }
 
-        CheckResult result = testCase.getDynamicInput().handle();
+        CheckResult result = runMain(testCase);
 
         if (result == null) {
             Throwable error = StageTest.getCurrTestRun().getErrorInTest();
-            try {
-                if (error == null) {
+
+            if (error == null) {
+                try {
                     return testCase.getCheckFunc().apply(
                         SystemOutHandler.getOutput(), testCase.getAttach());
+                } catch (Throwable ex) {
+                    error = ex;
+                    StageTest.getCurrTestRun().setErrorInTest(error);
                 }
-            } catch (Throwable ex) {
-                error = ex;
-                StageTest.getCurrTestRun().setErrorInTest(error);
             }
 
             if (error instanceof TestPassed) {
