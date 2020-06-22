2c48de1718f66c2900b31dabb7b738e7268015c0
inuur <inuur@mail.ru>
2020-06-22 13:54:25 +0300

    added rules to the test

--- src/test/java/outcomes/command_line_args/TestCommandLineArgumentsFailedWithSpaces.java
+++ src/test/java/outcomes/command_line_args/TestCommandLineArgumentsFailedWithSpaces.java
@@ -4,11 +4,29 @@ import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 import org.hyperskill.hstest.testcase.TestCase;
 import org.hyperskill.hstest.testing.TestedProgram;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 import java.util.Collections;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+
 public class TestCommandLineArgumentsFailedWithSpaces extends StageTest<String> {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void before() {
+        exception.expect(AssertionError.class);
+        exception.expectMessage(
+                "Arguments: -spaces \"some argument with spaces\" -number 234 -onlySpaces \"      \""
+        );
+        exception.expectMessage(not(containsString("Fatal error")));
+    }
 
     @Override
     public List<TestCase<String>> generate() {
