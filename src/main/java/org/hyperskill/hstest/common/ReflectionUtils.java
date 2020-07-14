e435a29085231c36e4877a6bc89035c909266f27
aaaaaa2493 <aaaaaa2493@yandex.ru>
2020-07-14 18:33:08 +0300

    Remove excess code

--- src/main/java/org/hyperskill/hstest/common/ReflectionUtils.java
+++ src/main/java/org/hyperskill/hstest/common/ReflectionUtils.java
@@ -1,9 +1,5 @@
 package org.hyperskill.hstest.common;
 
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtMethod;
-import javassist.NotFoundException;
 import org.hyperskill.hstest.exception.outcomes.FatalError;
 
 import java.lang.reflect.Method;
@@ -29,23 +25,5 @@ public final class ReflectionUtils {
         }
 
         return mainMethod;
-    }
-
-    public static int getLineNumber(Method method) {
-        int result = 0;
-
-        String methodName = method.getName();
-        String className = method.getDeclaringClass().getCanonicalName();
-        ClassPool pool = ClassPool.getDefault();
-
-        try {
-            CtClass cc = pool.get(className);
-            CtMethod ctMethod = cc.getDeclaredMethod(methodName);
-            result = ctMethod.getMethodInfo().getLineNumber(0);
-        } catch (NotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return result;
     }
 }
