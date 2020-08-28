abc4054722747e1858c7d5b5aecf7d3bf5cd2bde
Paulo Filipe <prnfilipe@gmail.com>
2020-08-28 20:30:40 +0100

    Reset

--- src/main/java/org/hyperskill/hstest/dynamic/output/SystemOutHandler.java
+++ src/main/java/org/hyperskill/hstest/dynamic/output/SystemOutHandler.java
@@ -5,66 +5,55 @@ import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testing.TestRun;
 
 import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 
 public final class SystemOutHandler {
-    
-    private SystemOutHandler() {
-    }
-    
+
+    private SystemOutHandler() { }
+
     private static final PrintStream REAL_OUT = System.out;
     private static final PrintStream REAL_ERR = System.err;
-    
+
     private static final SystemOutMock MOCK_OUT = new SystemOutMock(REAL_OUT);
     private static final SystemOutMock MOCK_ERR = new SystemOutMock(REAL_ERR);
-    
+
     public static PrintStream getRealOut() {
         return MOCK_OUT.getOriginal();
     }
-    
+
     public static PrintStream getRealErr() {
         return MOCK_ERR.getOriginal();
     }
-    
-    public static void replaceSystemOut() throws UnsupportedEncodingException {
-        System.setOut(new PrintStream(MOCK_OUT, true, "UTF-8"));
-        System.setErr(new PrintStream(MOCK_ERR, true, "UTF-8"));
+
+    public static void replaceSystemOut() {
+        System.setOut(new PrintStream(MOCK_OUT, true));
+        System.setErr(new PrintStream(MOCK_ERR, true));
     }
-    
+
     public static void revertSystemOut() {
         resetOutput();
         System.setOut(REAL_OUT);
         System.setErr(REAL_ERR);
     }
-    
+
     public static void resetOutput() {
         MOCK_OUT.reset();
         MOCK_ERR.reset();
     }
-    
+
     public static String getOutput() {
-        String output = new String(
-                MOCK_OUT.getClonedOut().toByteArray(),
-                StandardCharsets.UTF_8);
-        return Utils.cleanText(output);
+        return Utils.cleanText(MOCK_OUT.getClonedOut().toString());
     }
-    
+
     public static String getDynamicOutput() {
-        String output = new String(
-                MOCK_OUT.getDynamicOut().toByteArray(),
-                StandardCharsets.UTF_8);
-        return Utils.cleanText(output);
+        return Utils.cleanText(MOCK_OUT.getDynamicOut().toString());
     }
-    
+
     public static String getPartialOutput(ThreadGroup group) {
-        String output = new String(
-                MOCK_OUT.getPartialOut(group).toByteArray(),
-                StandardCharsets.UTF_8);
+        String output = Utils.cleanText(MOCK_OUT.getPartialOut(group).toString());
         MOCK_OUT.getPartialOut(group).reset();
-        return Utils.cleanText(output);
+        return output;
     }
-    
+
     public static void injectInput(String input) {
         TestRun testRun = StageTest.getCurrTestRun();
         if (testRun != null) {
