900950f8b85667851f3f5ae30f18b8a689dab692
Vladimir Turov <aaaaaa2493@yandex.ru>
2020-09-09 01:23:04 +0300

    Add test on exception inside catch block in the user code

---
+++ src/test/java/outcomes/lib/ExceptionInUserCode2.java
@@ -0,0 +1,70 @@
+package outcomes.lib;
+
+import org.hyperskill.hstest.stage.StageTest;
+import org.hyperskill.hstest.testcase.CheckResult;
+import org.hyperskill.hstest.testcase.TestCase;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+
+class ExceptionInUserCode2Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello World");
+            System.out.println(1 / 0);
+        } catch (Exception ex) {
+            System.out.println(1 / 0);
+        }
+    }
+}
+
+public class ExceptionInUserCode2 extends StageTest {
+
+    public ExceptionInUserCode2() {
+        super(ExceptionInUserCode2Main.class);
+    }
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void before() {
+        exception.expect(AssertionError.class);
+        exception.expectMessage(
+                "Exception in test #1\n" +
+                        "\n" +
+                        "java.lang.ArithmeticException: / by zero"
+        );
+        exception.expectMessage(
+                "Please find below the output of your program during this failed test.\n" +
+                        "\n" +
+                        "---\n" +
+                        "\n" +
+                        "Hello World"
+        );
+
+        exception.expectMessage(not(containsString("Unexpected error")));
+        exception.expectMessage(not(containsString("at org.hyperskill.hstest")));
+        exception.expectMessage(not(containsString("org.junit.")));
+        exception.expectMessage(not(containsString("at sun.reflect.")));
+        exception.expectMessage(not(containsString("at java.base/jdk.internal.reflect.")));
+    }
+
+    @Override
+    public List<TestCase> generate() {
+        return Arrays.asList(
+                new TestCase()
+        );
+    }
+
+    @Override
+    public CheckResult check(String reply, Object attach) {
+        return CheckResult.correct();
+    }
+}
--- src/test/java/outcomes/lib/TestNbspInOutput.java
+++ src/test/java/outcomes/lib/TestNbspInOutput.java
@@ -7,16 +7,16 @@ import org.hyperskill.hstest.testcase.TestCase;
 import java.util.Arrays;
 import java.util.List;
 
-class TextNbspInOutputMain {
+class TestNbspInOutputMain {
     public static void main(String[] args) {
         System.out.print("1\u00a02 3");
     }
 }
 
-public class TextNbspInOutput extends StageTest {
+public class TestNbspInOutput extends StageTest {
 
-    public TextNbspInOutput() {
-        super(TextNbspInOutputMain.class);
+    public TestNbspInOutput() {
+        super(TestNbspInOutputMain.class);
     }
 
     @Override
