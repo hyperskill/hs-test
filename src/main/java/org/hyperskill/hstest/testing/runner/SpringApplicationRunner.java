2de0798b073a1f084ef7612eb6d5b7870a3215df
Vladimir Turov <aaaaaa2493@yandex.ru>
2020-12-25 21:09:32 +0300

    Move launching Spring app to a separate method

--- src/main/java/org/hyperskill/hstest/testing/runner/SpringApplicationRunner.java
+++ src/main/java/org/hyperskill/hstest/testing/runner/SpringApplicationRunner.java
@@ -13,24 +13,29 @@ import org.hyperskill.hstest.testing.TestRun;
 import java.lang.reflect.InvocationTargetException;
 
 public class SpringApplicationRunner implements TestRunner {
+
+    private void launchSpringBootApplication(TestCase<?> testCase) {
+        String errorMessage = "Cannot start Spring application";
+        try {
+            SpringTest.main(testCase.getArgs().toArray(new String[0]));
+        } catch (InvocationTargetException ex) {
+            if (ex.getCause().getClass().getSimpleName().equals("PortInUseException")) {
+                errorMessage += "\nMake sure that no other Spring application is running at the moment.";
+            }
+            throw new ExceptionWithFeedback(errorMessage, ex.getCause());
+        } catch (OutcomeError error) {
+            throw error;
+        } catch (Throwable ex) {
+            throw new UnexpectedError(errorMessage, ex);
+        }
+    }
+
     @Override
     public CheckResult test(TestRun testRun) {
         TestCase<?> testCase = testRun.getTestCase();
 
         if (testRun.getTestNum() == 1) {
-            String errorMessage = "Cannot start Spring application";
-            try {
-                SpringTest.main(testCase.getArgs().toArray(new String[0]));
-            } catch (InvocationTargetException ex) {
-                if (ex.getCause().getClass().getSimpleName().equals("PortInUseException")) {
-                    errorMessage += "\nMake sure that no other Spring application is running at the moment.";
-                }
-                throw new ExceptionWithFeedback(errorMessage, ex.getCause());
-            } catch (OutcomeError error) {
-                throw error;
-            } catch (Throwable ex) {
-                throw new UnexpectedError(errorMessage, ex);
-            }
+            launchSpringBootApplication(testCase);
         }
 
         try {
