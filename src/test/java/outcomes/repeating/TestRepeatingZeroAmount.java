105260e13eacbb3e63ef54bd7730a4ad88db9bf2
Vladimir Turov <aaaaaa2493@yandex.ru>
2020-12-28 02:36:22 +0300

    Fix test

--- src/test/java/outcomes/repeating/TestRepeatingZeroAmount.java
+++ src/test/java/outcomes/repeating/TestRepeatingZeroAmount.java
@@ -3,9 +3,9 @@ package outcomes.repeating;
 import org.hyperskill.hstest.dynamic.DynamicTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 import outcomes.base.ContainsMessage;
-import outcomes.base.UserErrorTest;
+import outcomes.base.UnexpectedErrorTest;
 
-public class TestRepeatingZeroAmount extends UserErrorTest {
+public class TestRepeatingZeroAmount extends UnexpectedErrorTest {
     @ContainsMessage
     String s = "UnexpectedError: No tests found";
 
