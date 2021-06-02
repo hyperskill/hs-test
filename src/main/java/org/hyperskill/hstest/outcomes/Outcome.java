42ac66e905711aaa958bd17f31bc0d8284836d39
Ainur Gimadeev <inuur@mail.ru>
2021-06-02 12:30:28 +0300

    Outcome.toString() method refactor

--- src/main/java/org/hyperskill/hstest/outcomes/Outcome.java
+++ src/main/java/org/hyperskill/hstest/outcomes/Outcome.java
@@ -85,39 +85,37 @@ public abstract class Outcome {
             arguments = argumentsBuilder.toString().trim();
         }
 
+
+    if (worthShowingLog || arguments.length() > 0) {
+        result += "\n\n";
+        if (worthShowingLog) {
+            result += "Please find below the output of your program during this failed test.\n";
+            if (testRun != null && testRun.isInputUsed()) {
+                result += "Note that the '>' character indicates the beginning of the input line.\n";
+            }
+            result += "\n---\n\n";
+        }
+
+        if (arguments.length() > 0) {
+            result += arguments + "\n\n";
+        }
+
         int MAX_LINES_IN_OUTPUT = 250;
         String[] lines = fullLog.split("\n");
-
         boolean isOutputTooLong = lines.length > MAX_LINES_IN_OUTPUT;
 
-        if (worthShowingLog || arguments.length() > 0) {
-            result += "\n\n";
-            if (worthShowingLog) {
-                result += "Please find below the output of your program during this failed test.\n";
-                if (testRun != null && testRun.isInputUsed()) {
-                    result += "Note that the '>' character indicates the beginning of the input line.\n";
-                }
-                result += "\n---\n\n";
-            }
-
-            if (arguments.length() > 0) {
-                result += arguments + "\n\n";
-            }
-
+        if (worthShowingLog) {
             if (isOutputTooLong) {
-                result += "[last " + MAX_LINES_IN_OUTPUT + " lines of output are shown, " + (lines.length - MAX_LINES_IN_OUTPUT) + " skipped]\n";
-            }
-
-            if (worthShowingLog) {
-                if (isOutputTooLong) {
-                    String[] lastLines =
-                        Arrays.copyOfRange(lines, lines.length - MAX_LINES_IN_OUTPUT, lines.length);
-                    result += String.join("\n", lastLines);
-                } else {
-                    result += fullLog;
-                }
+                result += "[last " + MAX_LINES_IN_OUTPUT + " lines of output are shown, "
+                    + (lines.length - MAX_LINES_IN_OUTPUT) + " skipped]\n";
+                String[] lastLines =
+                    Arrays.copyOfRange(lines, lines.length - MAX_LINES_IN_OUTPUT, lines.length);
+                result += String.join("\n", lastLines);
+            } else {
+                result += fullLog;
             }
         }
+    }
 
         return result.trim();
     }
