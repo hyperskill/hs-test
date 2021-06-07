db607b16e65daad591a9b31f9544258ff4370039
Ainur Gimadeev <inuur@mail.ru>
2021-06-07 21:49:05 +0300

    Include stderr to feedback & refactor Outcome.toString() method

---
+++ src/test/java/outcomes/stderr_on_unexpected_error/without_args/TestOutputWithoutStderrAndWithoutStdout.java
@@ -0,0 +1,36 @@
+package outcomes.stderr_on_unexpected_error.without_args;
+
+import org.hyperskill.hstest.dynamic.DynamicTest;
+import org.hyperskill.hstest.testcase.CheckResult;
+import org.hyperskill.hstest.testing.TestedProgram;
+import outcomes.base.ContainsMessage;
+import outcomes.base.UnexpectedErrorTest;
+
+class TestOutputWithoutStderrAndWithoutStdoutMain {
+    public static void main(String[] args) {
+
+    }
+}
+
+public class TestOutputWithoutStderrAndWithoutStdout extends UnexpectedErrorTest<String> {
+
+    @ContainsMessage
+    String[] m = {
+        "Unexpected error in test #1",
+        "java.lang.ArithmeticException: / by zero"
+    };
+
+    String[] wrongLines = {
+        "stderr",
+        "stdout"
+    };
+
+    @DynamicTest
+    CheckResult test() {
+        TestedProgram testedProgram = new TestedProgram(TestOutputWithoutStderrAndWithoutStdoutMain.class);
+        testedProgram.start();
+        int a = 2 / 0;
+        return CheckResult.correct();
+    }
+
+}
