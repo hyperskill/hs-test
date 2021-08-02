3cc656a90665f86df696daa97fe99e6b20ade190
Vladimir Turov <aaaaaa2493@yandex.ru>
2021-08-02 12:41:18 +0300

    Revert using DynamicCLassLoader in Spring (now working)

--- src/main/java/org/hyperskill/hstest/stage/SpringTest.java
+++ src/main/java/org/hyperskill/hstest/stage/SpringTest.java
@@ -4,7 +4,6 @@ import org.apache.http.entity.ContentType;
 import org.hyperskill.hstest.common.FileUtils;
 import org.hyperskill.hstest.common.NetworkUtils;
 import org.hyperskill.hstest.common.ReflectionUtils;
-import org.hyperskill.hstest.dynamic.DynamicClassLoader;
 import org.hyperskill.hstest.dynamic.output.InfiniteLoopDetector;
 import org.hyperskill.hstest.dynamic.output.OutputHandler;
 import org.hyperskill.hstest.exception.outcomes.UnexpectedError;
@@ -138,9 +137,7 @@ public abstract class SpringTest extends StageTest<Object> {
 
     public static void startSpring() throws Exception {
         if (!springRunning) {
-            DynamicClassLoader loader = new DynamicClassLoader(springClass);
-            Class<?> runClass = loader.loadClass(springClass.getName());
-            Method mainMethod = ReflectionUtils.getMainMethod(runClass);
+            Method mainMethod = ReflectionUtils.getMainMethod(springClass);
             mainMethod.invoke(null, new Object[] {args});
             springRunning = true;
         }
