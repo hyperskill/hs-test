7e21e9ac479844fbca96b35b42b9205717757721
Vladimir Turov <aaaaaa2493@yandex.ru>
2021-09-01 15:48:47 +0300

    Fix test

--- src/test/java/outcomes/static_fields/TestUseDefaultClassLoader.java
+++ src/test/java/outcomes/static_fields/TestUseDefaultClassLoader.java
@@ -5,6 +5,7 @@ import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 import org.hyperskill.hstest.testing.TestedProgram;
 import org.hyperskill.hstest.testing.execution.MainMethodExecutor;
+import org.junit.Before;
 
 class TestUseDefaultClassLoaderMain {
     static int x = 0;
@@ -16,21 +17,28 @@ class TestUseDefaultClassLoaderMain {
 
 public class TestUseDefaultClassLoader extends StageTest {
 
+    int x;
+
     public TestUseDefaultClassLoader() {
         super(TestUseDefaultClassLoaderMain.class);
+    }
+
+    @Before
+    public void setup() {
+        x = TestUseDefaultClassLoaderMain.x;
     }
 
     @DynamicTest
     CheckResult test1() {
         TestedProgram pr = new TestedProgram(TestUseDefaultClassLoaderMain.class);
         ((MainMethodExecutor) pr.getProgramExecutor()).setUseSeparateClassLoader(false);
-        return new CheckResult(pr.start().equals("1"), "");
+        return new CheckResult(pr.start().equals("" + (x+1)), "");
     }
 
     @DynamicTest
     CheckResult test2() {
         TestedProgram pr = new TestedProgram(TestUseDefaultClassLoaderMain.class);
         ((MainMethodExecutor) pr.getProgramExecutor()).setUseSeparateClassLoader(false);
-        return new CheckResult(pr.start().equals("2"), "");
+        return new CheckResult(pr.start().equals("" + (x+2)), "");
     }
 }
