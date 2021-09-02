c81c2394ee71f78cab4f525be883aadeddf179c3
Vladimir Turov <aaaaaa2493@yandex.ru>
2021-09-02 22:03:27 +0300

    Add comments to methods in Util class

--- src/main/java/org/hyperskill/hstest/common/Utils.java
+++ src/main/java/org/hyperskill/hstest/common/Utils.java
@@ -55,6 +55,9 @@ public final class Utils {
         return NetworkUtils.getUrlPage(url);
     }
 
+    /**
+     * Cleans text from non-breaking spaces and sets "\n" as a line ending.
+     */
     public static String cleanText(String str) {
         String nbsp = "\u00a0";
         String space = "\u0020";
@@ -64,11 +67,37 @@ public final class Utils {
             .replace(nbsp, space);
     }
 
+    /**
+     * Capitalizes the first character of the string
+     * Examples:
+     * 1) "name" -> "Name"
+     * 2) "maiN" -> "MaiN"
+     */
     public static String capitalize(String str) {
         return str.substring(0, 1).toUpperCase()
             + (str.length() > 1 ? str.substring(1) : "");
     }
 
+    /**
+     * Numbers-aware compare.
+     * Compare strings as strings, but numbers inside strings as numbers.
+     *
+     * Examples:
+     * 1) name0 < name1 < name2 < ... < name9 < name10 < name11 ...
+     * 2) test_8_analyze < test_9_check < test_10_execute ...
+     * 3) test5_8 < test5_9 < test5_10 < ... < test6_1 < test6_9 < test6_10 ...
+     *
+     * But keep in mind, that "test_a_9" < "test_b_8"
+     * Since "test_a_" < "test_b_".
+     * Numbers are compared only if the preceding substrings are equal.
+     *
+     * @param s1 First string to compare
+     * @param s2 Second string to compare
+     * @return
+     *      0 if s1 = s2,
+     *      >0 if s1 > s2,
+     *      <0 if s2 > s1
+     */
     public static int smartCompare(String s1, String s2) {
 
         class NamePair implements Comparable<NamePair> {
