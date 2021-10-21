6c81288cc66786da168ad68cbbb25e2504327967
Vladimir Turov <aaaaaa2493@yandex.ru>
2021-10-21 22:32:41 +0300

    Add 2 more tests on stderr

---
+++ src/test/java/outcomes/lib/OutputErrInTests2.java
@@ -0,0 +1,49 @@
+package outcomes.lib;
+
+import org.hyperskill.hstest.dynamic.DynamicTest;
+import org.hyperskill.hstest.testcase.CheckResult;
+import org.hyperskill.hstest.testing.TestedProgram;
+import outcomes.base.ContainsMessage;
+import outcomes.base.UserErrorTest;
+
+import java.util.Scanner;
+
+class OutputErrInTests2Main {
+    public static void main(String[] args) {
+        System.out.println("123");
+        new Scanner(System.in).nextLine();
+        System.out.println("456");
+    }
+}
+
+public class OutputErrInTests2 extends UserErrorTest {
+
+    @ContainsMessage
+    String msg =
+        "Wrong answer in test #1\n" +
+        "\n" +
+        "Please find below the output of your program during this failed test.\n" +
+        "Note that the '>' character indicates the beginning of the input line.\n" +
+        "\n" +
+        "---\n" +
+        "\n" +
+        "123\n" +
+        "> 789\n" +
+        "456";
+
+    @DynamicTest
+    CheckResult test() {
+        var pr = new TestedProgram(OutputInTestsMain.class);
+        var out = pr.start();
+
+        if (!out.equals("123\n")) {
+            return CheckResult.wrong("");
+        }
+
+        System.err.println("Test output");
+        pr.execute("789");
+
+        return CheckResult.wrong("");
+    }
+
+}
