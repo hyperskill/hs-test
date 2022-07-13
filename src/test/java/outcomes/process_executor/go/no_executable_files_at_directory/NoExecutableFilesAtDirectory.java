b9ff104a7912500ed5e343360c61c5cbaa0cd0a0
tanya <melnikova.ta@phystech.edu>
2022-07-13 16:46:25 +0300

    Add stopProcessTest function to process executor tests

--- src/test/java/outcomes/process_executor/go/no_executable_files_at_directory/NoExecutableFilesAtDirectory.java
+++ src/test/java/outcomes/process_executor/go/no_executable_files_at_directory/NoExecutableFilesAtDirectory.java
@@ -3,11 +3,20 @@ package outcomes.process_executor.go.no_executable_files_at_directory;
 import org.hyperskill.hstest.dynamic.DynamicTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 import org.hyperskill.hstest.testing.TestedProgram;
+import org.junit.Assume;
+import org.junit.BeforeClass;
 import outcomes.base.ContainsMessage;
 import outcomes.base.UserErrorTest;
 
+import static org.hyperskill.hstest.testing.ExecutionOptions.includeProcessTesting;
+
 
 public class NoExecutableFilesAtDirectory extends UserErrorTest<String> {
+
+    @BeforeClass
+    public static void stopProcessTest() {
+        Assume.assumeTrue(includeProcessTesting);
+    }
 
     @ContainsMessage
     String m1 = "Cannot find a file to execute your code.\nAre your project files located at";
