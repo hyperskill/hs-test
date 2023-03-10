b163c4e66b4a5f1396feea6a981dd8fe42314f94
home1 <polischuk0107@gmail.com>
2023-03-11 00:08:21 +0200

    Fixed startSpring method

--- src/main/java/org/hyperskill/hstest/stage/SpringTest.java
+++ src/main/java/org/hyperskill/hstest/stage/SpringTest.java
@@ -182,7 +182,7 @@ public abstract class SpringTest extends StageTest<Object> {
                 List<Class<?>> classes = ReflectionUtils.getAllClassesFromPackage("");
                 classes.forEach(it -> {
                             if (it.getCanonicalName().contains("ApplicationKt")
-                                    /*&& it.getDeclaredClasses().equals("main")*/) {
+                                    && it.getDeclaredMethods().equals("main")) {
                                 try {
                                     ReflectionUtils.getMainMethod(it)
                                         .invoke(null, new Object[]{args});
